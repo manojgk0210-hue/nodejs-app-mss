8dede5ef68c454630f14af4344eec6b01e3d63fb
Manoj G K <manojgk0210@gmail.com>
2026-06-14 10:59:16 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,17 +1,13 @@
 const scanner = require('sonarqube-scanner');
 
-sonarqubeScanner({
+scanner.default({
   serverUrl: 'http://13.126.122.232:9000/',
-       options : {
-	    'sonar.projectDescription': 'This is a Node JS application',
-	    'sonar.projectName': 'Node JS Application - Sample',
-	    'sonar.projectKey':'NodeJsMSS',
-	    'sonar.login': 'squ_d9c9946a0c6fd9b02a0caeb93a38c1e174c4f360',
-            'sonar.projectVersion':'1.0',
-	    'sonar.language':'js',
-            'sonar.sourceEncoding':'UTF-8',
-            'sonar.sources': '.',
-	  //'sonar.tests': 'specs',
-          //'sonar.inclusions' : 'src/**'
-       },
+  options: {
+    'sonar.projectDescription': 'This is a Node JS application',
+    'sonar.projectName': 'Node JS Application - Sample',
+    'sonar.projectKey': 'NodeJsMSS',
+    'sonar.token': 'squ_d9c9946a0c6fd9b02a0caeb93a38c1e174c4f360',
+    'sonar.projectVersion': '1.0',
+    'sonar.sources': '.'
+  }
 }, () => {});
